a2d811b7be1f8bc0e590d795789e50705060fdb7
leoneldagostino <leeonel_@live.com>
2023-04-28 23:14:04 -0300

    refactorizacion

--- desafioStark/industriaStark01.py
+++ desafioStark/industriaStark01.py
@@ -1,49 +1,51 @@
 from data_stark import lista_personajes
 
 
-def imprimir_heroe_genero(lista,genero:str):
-    for heroe in lista:
-        if genero == heroe["genero"]:
-            print("El hereo: {}".format(heroe["nombre"]))
+def imprimir_lista(lista):
+    for elemento in lista:
+        print("El hereo: {} - genero: {}".format(elemento["nombre"],elemento["genero"]))
     
                 
-        
-# imprimir_hereo_genero(lista_personajes,"M")
-# imprimir_hereo_genero(lista_personajes,"F")
+def retornar_lista_por_genero(lista:list,genero:str):
+    '''
+    Returna una lista filtrada por genero recibiendo la lista a filtrar como parametro
+    '''
+    lista_genero = []
+    for elemento in lista:
+        if genero == elemento["genero"]:
+            lista_genero.append(elemento)
+    return lista_genero
 
-def imprimir_hereo_mas_alto(genero:str):
-    print("El superhereo mas alto es: ",end='\n')
-    hereo_mas_alto = float(lista_personajes[0]['altura'])
-    indice_heroe_mas_alto = lista_personajes[0]
+lista_hereo_masculinos = retornar_lista_por_genero(lista_personajes,"M")
+imprimir_lista(lista_hereo_masculinos)
+lista_hereo_femenino = retornar_lista_por_genero(lista_personajes,"F")
+imprimir_lista(lista_hereo_femenino)
+
+
+def imprimir_hereo_mas_alto(lista:list):
+    hereo_mas_alto = float(lista[0]['altura'])
+    indice_heroe_mas_alto = lista[0]
 
     
     #buscamos el mas alto
-    for heroe in lista_personajes:
-        if genero == heroe["genero"]:
-            altura = float(heroe["altura"])
-            if altura > hereo_mas_alto:
-                indice_heroe_mas_alto = heroe
-                hereo_mas_alto = altura
+    for heroe in lista:
+        altura = float(heroe["altura"])
+        if altura > hereo_mas_alto:
+            indice_heroe_mas_alto = heroe
+            hereo_mas_alto = altura
 
 
     #imprimimos el nombre y altura del hereo mas alto
     return "El mas alto es: {} - {} \n".format(indice_heroe_mas_alto['identidad'],hereo_mas_alto)
 
 
-# imprimir_hereo_mas_alto("F")
-# imprimir_hereo_mas_alto("M")
 
-def imprimir_hereo_mas_bajo(genero:str):
-    print("El superhereo mas bajo es: ",end='\n')
-    lista_heroes = []
+def imprimir_hereo_mas_bajo(lista:list):
 
-    for heroe in lista_personajes:
-        if genero == heroe["genero"]:
-            lista_heroes.append(heroe)
-    hereo_mas_bajo = float(lista_heroes[0]['altura'])
-    indice_heroe_mas_bajo = lista_heroes[0]
+    hereo_mas_bajo = float(lista[0]['altura'])
+    indice_heroe_mas_bajo = lista[0]
     #buscamos el mas bajo
-    for heroe in lista_heroes:            
+    for heroe in lista:            
         altura = float(heroe["altura"])
         if altura < hereo_mas_bajo:
             indice_heroe_mas_bajo = heroe
@@ -51,69 +53,47 @@ def imprimir_hereo_mas_bajo(genero:str):
 
 
     #imprimimos el nombre y altura del hereo mas bajo
-    if genero == "F":
-        return "El mas bajo femenino es: {} - {} \n".format(indice_heroe_mas_bajo['identidad'],hereo_mas_bajo)
-    elif genero == "M":
-        return "El mas bajo masculino es: {} - {} \n".format(indice_heroe_mas_bajo['identidad'],hereo_mas_bajo)
+    return "El mas bajo es: {} - {} \n".format(indice_heroe_mas_bajo['identidad'],hereo_mas_bajo)
 
 
-# imprimir_hereo_mas_bajo("F")
-# imprimir_hereo_mas_bajo("M")
-
-def imprimir_promedio_altura(genero:str):
-    lista_hereos = []
-    for hereo in lista_personajes:
-        if hereo["genero"] == genero:
-            lista_hereos.append(hereo)
-            
+def imprimir_promedio_altura(lista:list):
     altura_total_hereos = 0
-    numero_hereos= len(lista_hereos)
-    for hereo in lista_hereos:
+    numero_hereos= len(lista)
+    for hereo in lista:
         altura = float(hereo["altura"])
         altura_total_hereos += altura
         
 
     promedio_altura = float(altura_total_hereos / numero_hereos)
-    if genero == "F":
-        return"El promedio de altura de los hereo femeninos es: {} \n".format(promedio_altura)
-    else:
-        return "El promedio de altura de los hereos masculino es: {} \n".format(promedio_altura)
+    return"El promedio de altura de los heroes es: {} \n".format(promedio_altura)
         
 
-# imprimir_promedio_altura("F")
-# imprimir_promedio_altura("M")
 
-
-def contador_hereos_color_ojos():
+def contador_hereos_color_ojos(lista:list):
     tipos_color_ojos = {}
-    for heroe in lista_personajes:
+    for heroe in lista:
         if heroe["color_ojos"] not in tipos_color_ojos:
             tipos_color_ojos.update({heroe["color_ojos"]:1})
         else:
             tipos_color_ojos[heroe["color_ojos"]] += 1
-    for tipo,valor in tipos_color_ojos.items():
-        print("{}: {}".format(tipo,valor))
+    return tipos_color_ojos
 
-contador_hereos_color_ojos()                    
-#contador_hereos_color_ojos()
 
-def contador_hereos_color_pelo():
+def contador_hereos_color_pelo(lista:list):
     tipos_color_pelo = {}
-    for heroe in lista_personajes:
+    for heroe in lista:
         if heroe["color_pelo"] not in tipos_color_pelo:
             if heroe["color_pelo"] == "":
                 continue
             tipos_color_pelo.update({heroe["color_pelo"]:1})
         else:
             tipos_color_pelo[heroe["color_pelo"]] += 1
-    for tipo,valor in tipos_color_pelo.items():
-        print("{}: {}".format(tipo,valor))
+    return tipos_color_pelo
  
-# contador_hereos_color_pelo()
 
-def contador_tipo_inteligencia():
+def contador_tipo_inteligencia(lista:list):
     tipos_inteligencia = {}
-    for heroe in lista_personajes:
+    for heroe in lista:
         if heroe["inteligencia"] not in tipos_inteligencia:
             if heroe["inteligencia"] == "":
                 tipos_inteligencia.update({"No tiene":1})
@@ -121,27 +101,23 @@ def contador_tipo_inteligencia():
             tipos_inteligencia.update({heroe["inteligencia"]:1})
         else:
             tipos_inteligencia[heroe["inteligencia"]] += 1
-    for tipo,valor in tipos_inteligencia.items():
-        print("{}: {}".format(tipo,valor))
+    return tipos_inteligencia
         
-# contador_tipo_inteligencia()
 
-def agrupador_color_ojos():
+def agrupador_color_ojos(lista:list):
     agrupacion_heroe_por_color_ojos = {}
-    for heroe in lista_personajes:
+    for heroe in lista:
         if heroe["color_ojos"] not in agrupacion_heroe_por_color_ojos:
             agrupacion_heroe_por_color_ojos.update({heroe["color_ojos"]:[heroe["nombre"]]})
         else:
             corregimos_lista = agrupacion_heroe_por_color_ojos[heroe["color_ojos"]] 
             corregimos_lista.append(heroe["nombre"])
             agrupacion_heroe_por_color_ojos[heroe["color_ojos"]] = corregimos_lista
-    for tipo,valor in agrupacion_heroe_por_color_ojos.items():
-        print("{}: {}".format(tipo,valor))
-
+    return agrupacion_heroe_por_color_ojos
     
-def agrupador_color_pelo():
+def agrupador_color_pelo(lista:list):
     agrupacion_heroe_por_color_pelo = {}
-    for heroe in lista_personajes:
+    for heroe in lista:
         if heroe["color_pelo"] not in agrupacion_heroe_por_color_pelo:
             if heroe["color_pelo"] == "":
                 continue
@@ -150,12 +126,11 @@ def agrupador_color_pelo():
             corregimos_lista = agrupacion_heroe_por_color_pelo[heroe["color_pelo"]] 
             corregimos_lista.append(heroe["nombre"])
             agrupacion_heroe_por_color_pelo[heroe["color_pelo"]] = corregimos_lista
-    for tipo,valor in agrupacion_heroe_por_color_pelo.items():
-        print("{}: {}".format(tipo,valor))
+    return agrupacion_heroe_por_color_pelo
         
-def agrupador_inteligencia():
+def agrupador_inteligencia(lista:list):
     agrupacion_heroe_por_inteligencia = {}
-    for heroe in lista_personajes:
+    for heroe in lista:
         if heroe["inteligencia"] not in agrupacion_heroe_por_inteligencia:
             if heroe["inteligencia"] == "":
                 agrupacion_heroe_por_inteligencia.update({"No tiene":[heroe["nombre"]]})
@@ -165,10 +140,8 @@ def agrupador_inteligencia():
             corregimos_lista = agrupacion_heroe_por_inteligencia[heroe["inteligencia"]] 
             corregimos_lista.append(heroe["nombre"])
             agrupacion_heroe_por_inteligencia[heroe["inteligencia"]] = corregimos_lista
-    for tipo,valor in agrupacion_heroe_por_inteligencia.items():
-        print("{}: {}".format(tipo,valor))
+    return agrupacion_heroe_por_inteligencia
 
-# agrupador_inteligencia()
 
 
 
